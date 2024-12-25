31a9380855b947eea8be229d29da51be1fac6b78
Nguyen Thanh Dat <datnt5118@ut.edu.vn>
2024-12-25 22:45:26 +0700

    update

--- web-dat-lich-kham-benh/app/urls.py
+++ web-dat-lich-kham-benh/app/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('gioithieu/', views.gioithieu, name="gioithieu"),
+    path('dichvu/', views.dichvu, name="dichvu"),
+    path('banggia/', views.banggia, name="banggia"),
+    path('tintuc/', views.tintuc, name="tintuc"),
 ]
--- web-dat-lich-kham-benh/app/views.py
+++ web-dat-lich-kham-benh/app/views.py
@@ -3,4 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'app/home.html')
+    context= {}
+    return render(request,'app/home.html')
+def cart(request):
+    context= {}
+    return render(request,'app/cart.html',context)
+def checkout(request):
+    context= {}
+    return render(request,'app/checkout.html',context)
+def gioithieu(request):
+    context= {}
+    return render(request,'app/gioithieu.html',context)
+def dichvu(request):
+    context= {}
+    return render(request,'app/dichvu.html',context)
+def banggia(request):
+    context= {}
+    return render(request,'app/banggia.html',context)
+def tintuc(request):
+    context= {}
+    return render(request,'app/tintuc.html',context)
