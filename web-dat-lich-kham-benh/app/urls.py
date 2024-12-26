96f9b7bd0d5b2c5e2349ee7ac5c2c0491de60fe9
Quỳnh Mai <ntqm1106@gmail.com>
2024-12-26 22:49:37 +0700

    update

--- web-dat-lich-kham-benh/app/urls.py
+++ web-dat-lich-kham-benh/app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
-    path('register/', views.register, name='register'),
+    path('register/', views.register, name="register"),
 
 ]
